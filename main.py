f48b0fb358155f5102a70c27881e4d4de622d6cf
RadySonabu <ardynubanos@gmail.com>
2023-08-12 22:38:49 +0800

    Update function detect_object

--- main.py
+++ main.py
@@ -19,42 +19,45 @@ output_folder = "test_folder"
 async def root():
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
 
-@app.post("/detect_objects/")
-async def detect_objects(file: UploadFile = File(...)):
+@app.get("/detect_objects/")
+async def detect_objects():
     try:
-        image_data = await file.read()
-        buf = BytesIO(image_data)
-        detected_objects, annotated_image = detect_objects_on_image(buf, model, output_folder)
-        annotated_image_data = image_to_base64(annotated_image)
+        # image_data = await file.read()
 
-        return JSONResponse(content={"detected_objects": detected_objects, "annotated_image": annotated_image_data})
+        # buf = BytesIO(image_data)
+        # detected_objects, annotated_image = detect_objects_on_image(buf, model, output_folder)
+        # annotated_image_data = image_to_base64(annotated_image)
+
+        return JSONResponse(content={'hey': model})
+        # return JSONResponse(content={"detected_objects": detected_objects, "annotated_image": annotated_image_data})
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
 
-def detect_objects_on_image(buf, model, output_folder):
-    temp_image = Image.open(buf)
-    results = model.predict(temp_image)
-    result = results[0]
-    output = []
-    for box in result.boxes:
-        x1, y1, x2, y2 = [round(x) for x in box.xyxy[0].tolist()]
-        class_id = box.cls[0].item()
-        prob = round(box.conf[0].item(), 2)
-        output.append([x1, y1, x2, y2, result.names[class_id], prob])
+# def detect_objects_on_image(buf, model, output_folder):
+#     temp_image = Image.open(buf)
+#     results = model.predict(temp_image)
+#     result = results[0]
+#     output = []
+#     for box in result.boxes:
+#         x1, y1, x2, y2 = [round(x) for x in box.xyxy[0].tolist()]
+#         class_id = box.cls[0].item()
+#         prob = round(box.conf[0].item(), 2)
+#         output.append([x1, y1, x2, y2, result.names[class_id], prob])
 
-        cropped_image = temp_image.crop((x1, y1, x2, y2))
-        image_filename = f"{result.names[class_id]}_{prob}.jpg"
-        cropped_image.save(os.path.join(output_folder, image_filename))
+#         cropped_image = temp_image.crop((x1, y1, x2, y2))
+#         image_filename = f"{result.names[class_id]}_{prob}.jpg"
+#         cropped_image.save(os.path.join(output_folder, image_filename))
 
-    image = temp_image.copy()
-    draw = ImageDraw.Draw(image)
-    for box in output:
-        x1, y1, x2, y2, _, _ = box
-        draw.rectangle([x1, y1, x2, y2], outline='red', width=2)
+#     image = temp_image.copy()
+#     draw = ImageDraw.Draw(image)
+#     for box in output:
+#         x1, y1, x2, y2, _, _ = box
+#         draw.rectangle([x1, y1, x2, y2], outline='red', width=2)
 
-    return output, image
+#     return output, image
 
-def image_to_base64(image):
-    buffered = BytesIO()
-    image.save(buffered, format="JPEG")
-    return base64.b64encode(buffered.getvalue()).decode()
+# def image_to_base64(image):
+
+#     buffered = BytesIO()
+#     image.save(buffered, format="JPEG")
+#     return base64.b64encode(buffered.getvalue()).decode()
