c0cc6947eaf288aec4806b3b3ec11840695bdf85
RadySonabu <ardynubanos@gmail.com>
2023-08-14 02:31:58 +0800

    update code

--- main.py
+++ main.py
@@ -1,37 +1,37 @@
 from fastapi import FastAPI
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
-from PIL import Image, ImageDraw
-from io import BytesIO
-from ultralytics import YOLO
-import os
-import base64
-import cv2
+# from PIL import Image, ImageDraw
+# from io import BytesIO
+# from ultralytics import YOLO
+# import os
+# import base64
+# import cv2
 
 app = FastAPI()
 
-model_url = 'best.pt'
-model = YOLO(model_url)
+# model_url = 'best.pt'
+# model = YOLO(model_url)
 
-output_folder = "test_folder"
-
+# output_folder = "test_folder"
+model = 'asdf'
 @app.get("/")
 async def root():
     return {"greeting": "Hello, World!", "message": f"Welcome to FastAPI! {model}"}
 
-@app.get("/detect_objects/")
-async def detect_objects():
-    try:
-        # image_data = await file.read()
+# @app.get("/detect_objects/")
+# async def detect_objects():
+#     try:
+#         # image_data = await file.read()
 
-        # buf = BytesIO(image_data)
-        # detected_objects, annotated_image = detect_objects_on_image(buf, model, output_folder)
-        # annotated_image_data = image_to_base64(annotated_image)
+#         # buf = BytesIO(image_data)
+#         # detected_objects, annotated_image = detect_objects_on_image(buf, model, output_folder)
+#         # annotated_image_data = image_to_base64(annotated_image)
 
-        return JSONResponse(content={'hey': model})
-        # return JSONResponse(content={"detected_objects": detected_objects, "annotated_image": annotated_image_data})
-    except Exception as e:
-        return JSONResponse(content={"error": str(e)}, status_code=500)
+#         return JSONResponse(content={'hey': model})
+#         # return JSONResponse(content={"detected_objects": detected_objects, "annotated_image": annotated_image_data})
+#     except Exception as e:
+#         return JSONResponse(content={"error": str(e)}, status_code=500)
 
 # def detect_objects_on_image(buf, model, output_folder):
 #     temp_image = Image.open(buf)
